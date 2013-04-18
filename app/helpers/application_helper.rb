52862a5285d4267e7f3c93386d5885eb851dd8f0
Viktor Fonic <viktor.fonic@gmail.com>
2013-04-19 00:15:21 +0200

    Add facebook and twitter share buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def facebook_like
+    content_tag :iframe, nil, src: "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=350&action=recommend&font=tahoma&colorscheme=light&height=28", scrolling: 'no', frameborder: '0', allowtransparency: true, id: :facebook_like, style: "width: 350px; height: 60px"
+  end
 end
